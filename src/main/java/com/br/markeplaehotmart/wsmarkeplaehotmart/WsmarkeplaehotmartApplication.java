1800fbc106fe3757fd4e892806b6f3f3479ad6f5
guidu <guidu.83@gmail.com>
2021-06-02 17:42:55 -0300

    Product Service
    Product search sorted by hacking by name and category

---
+++ src/main/java/com/br/markeplaehotmart/wsmarkeplaehotmart/WsmarkeplaehotmartApplication.java
@@ -0,0 +1,13 @@
+package com.br.markeplaehotmart.wsmarkeplaehotmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsmarkeplaehotmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsmarkeplaehotmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/markeplaehotmart/wsmarkeplaehotmart/WsmarkeplaehotmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.markeplaehotmart.wsmarkeplaehotmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsmarkeplaehotmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
