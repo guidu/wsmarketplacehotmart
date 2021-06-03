9ab3e5cfddca4f5cf75da0d927524b56c9c5b565
guidu <guidu.83@gmail.com>
2021-06-03 01:00:21 -0300

    Update class

--- src/main/java/com/br/wsmarkeplaehotmart/WsmarkeplaehotmartApplication.java
+++ src/main/java/com/br/wsmarkeplaehotmart/WsmarkeplaehotmartApplication.java
@@ -1,4 +1,4 @@
-package com.br.markeplaehotmart.wsmarkeplaehotmart;
+package com.br.wsmarkeplaehotmart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
